93b954b0a9037c8f025d222c3d978bed1bacea8d
nadavsklar <sklar@post.bgu.ac.il>
2020-04-06 23:55:09 +0300

    priorities test

---
+++ xv6-public/prioritytest.c
@@ -0,0 +1,46 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int childpid = fork();
+	int secchildpid;
+
+	if (childpid == 0)
+	{
+		childpid = getpid();
+		set_ps_priority(1);
+		for (i = 0; i < 100; i++)
+		{
+			printf(STDOUT,"My pid is: %d priority: 1\n", childpid);
+		}
+		exit(0);
+	}
+	else
+	{
+		secchildpid = fork();
+		if (secchildpid == 0)
+		{
+			childpid = getpid();
+			set_ps_priority(5);
+			for (i = 0; i < 100; i++)
+			{
+			printf(STDOUT,"My pid is: %d priority: 1\n", childpid);
+			}
+			exit(0);
+		}
+		else
+		{
+			childpid = getpid();
+			set_ps_priority(10);
+			for (i = 0; i < 100; i++)
+			{
+				printf(STDOUT,"My pid is: %d priority: 1\n", childpid);
+			}
+			wait(NULL);
+			wait(NULL);
+			exit(0);
+		}
+	}
+}
